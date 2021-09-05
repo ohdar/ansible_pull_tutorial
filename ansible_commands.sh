0e8b0ef77722ab9d9749603be28dbd198e55c52c
Brajesh Ohdar <ohdar@live.com>
2021-09-05 23:30:08 +0530

    local.yml modified

--- ansible_commands.sh
+++ ansible_commands.sh
@@ -1,4 +1,4 @@
-sudo apt update
-sudo apt install git
-sudo apt install ansible
-sudo ansible-pull -U https://github.com/ohdar/ansible_pull_tutorial.git
+# sudo apt update
+# sudo apt install git
+# sudo apt install ansible
+# sudo ansible-pull -U https://github.com/ohdar/ansible_pull_tutorial.git
