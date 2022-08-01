4e96966ca7ade881989e0c565257ebca1ffc793a
Brajesh Ohdar <ohdar@live.com>
2022-08-02 01:01:41 +0530

    microsoft edge installation task added

--- ansible_commands.sh
+++ ansible_commands.sh
@@ -1,5 +1,5 @@
-# sudo apt update
-# sudo apt install git
-# sudo apt install ansible
-# sudo ansible-pull -U https://github.com/ohdar/ansible_pull_tutorial.git
-# /usr/bin/ansible-pull -U https://github.com/ohdar/ansible_pull_tutorial.git > /dev/null
+sudo apt update
+sudo apt install git
+sudo apt install ansible
+sudo ansible-pull -U https://github.com/ohdar/ansible_pull_tutorial.git
+/usr/bin/ansible-pull -U https://github.com/ohdar/ansible_pull_tutorial.git > /dev/null
