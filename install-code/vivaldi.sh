b8ceb74127c8e601b0c06e59d3ecaf518adeefb6
Brajesh Ohdar <ohdar@live.com>
2022-08-03 21:03:49 +0530

    installation sh files added

---
+++ install-code/vivaldi.sh
@@ -0,0 +1,7 @@
+sudo apt install wget
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
+echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
+sudo apt update 
+sudo apt install vivaldi-stable
+
+# sudo apt remove vivaldi-stable
